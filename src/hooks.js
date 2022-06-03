5dddaca92f21da772502546895a4fe46e094b817
Tracey <ttrepper@yahoo.com>
2022-06-03 08:12:21 -0400

    add exercise

---
+++ src/hooks.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import axios from "axios";
+
+function useFlip(initialFlipState = true) {
+  const [isFlipped, setFlipped] = useState(initialFlipState);
+  const flip = () => {
+    setFlipped((isUp) => !isUp);
+  };
+  return [isFlipped, flip];
+}
+function useAxios(baseUrl) {
+  const [responses, setResponses] = useState([]);
+
+  const addResponseData = async (
+    formatter = (data) => data,
+    restOfUrl = ""
+  ) => {
+    const response = await axios.get(`${baseUrl}${restOfUrl}`);
+    setResponses((data) => [...data, formatter(response.data)]);
+  };
+  const clearResponses = () => setResponses([]);
+
+  return [responses, addResponseData, clearResponses];
+}
+
+export { useFlip, useAxios };
